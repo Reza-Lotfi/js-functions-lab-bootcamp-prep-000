a2c2beeb0f0e29d16a738feda856449e552681da
Reza Lotfi <rezaiscool7@aol.com>
2018-07-29 07:34:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ function happyHolidaysTo(string){
 function happyHolidayTo(holiday, name){
   return(`Happy ${holiday}, ${name}!`)
 }
+function holidayCountdown(days, holiday){
+  return(`It's ${days} days until ${holiday}!`)
+}
